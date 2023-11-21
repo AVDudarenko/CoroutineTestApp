154b14e15d2100c523653b85e661365110a6cc63
aliaksandr <aleksandr.dudarenko@gmail.com>
2023-11-21 16:21:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/coroutinetestapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.coroutinetestapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.example.coroutinetestapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding.buttonLoad.setOnClickListener {
+            lifecycleScope.launch {
+                loadData()
+            }
+        }
+    }
+
+    private suspend fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temperature = loadTemperature(city)
+        binding.tvTemperature.text = temperature.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+    }
+
+
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Minsk"
+    }
+
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        delay(5000)
+        return -1
+    }
+}
