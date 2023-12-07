2edd7994b2a2a8dca827f8431d2ec20d48d5117c
Aliaksandr <dyvedd@gmail.com>
2023-12-07 10:56:45 +0300

    add job for load data

--- app/src/main/java/com/example/coroutinetestapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetestapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.coroutinetestapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
@@ -11,44 +10,52 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+	private val binding by lazy {
+		ActivityMainBinding.inflate(layoutInflater)
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch {
-                loadData()
-            }
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		binding.buttonLoad.setOnClickListener {
+			binding.progress.isVisible = true
+			binding.buttonLoad.isEnabled = false
+			val jobCity = lifecycleScope.launch {
+				val city = loadCity()
+				binding.tvLocation.text = city
+			}
+			val jobTemperature = lifecycleScope.launch {
+				val temperature = loadTemperature()
+				binding.tvTemperature.text = temperature.toString()
+			}
+			lifecycleScope.launch {
+				jobCity.join()
+				jobTemperature.join()
+				binding.progress.isVisible = false
+				binding.buttonLoad.isEnabled = true
+			}
+		}
+	}
 
-    private suspend fun loadData() {
-        binding.progress.isVisible = true
-        binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-        binding.tvLocation.text = city
-        val temperature = loadTemperature(city)
-        binding.tvTemperature.text = temperature.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
-    }
+	private suspend fun loadData() {
+		binding.progress.isVisible = true
+		binding.buttonLoad.isEnabled = false
+		val city = loadCity()
+		binding.tvLocation.text = city
+		val temperature = loadTemperature()
+		binding.tvTemperature.text = temperature.toString()
+		binding.progress.isVisible = false
+		binding.buttonLoad.isEnabled = true
+	}
 
 
-    private suspend fun loadCity(): String {
-        delay(5000)
-        return "Minsk"
-    }
+	private suspend fun loadCity(): String {
+		delay(2000)
+		return "Minsk"
+	}
 
-    private suspend fun loadTemperature(city: String): Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
-        delay(5000)
-        return -1
-    }
+	private suspend fun loadTemperature(): Int {
+		delay(5000)
+		return -1
+	}
 }
